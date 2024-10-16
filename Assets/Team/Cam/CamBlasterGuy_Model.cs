4f710fd6dc6257be28446909f3904c2e7e5f81e4
Cameron Bonde <CameronBonde@users.noreply.github.com>
2024-10-16 14:25:29 +1100

    no message

--- Assets/Team/Cam/CamBlasterGuy_Model.cs
+++ Assets/Team/Cam/CamBlasterGuy_Model.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamBlasterGuy_Model : MonoBehaviour
+namespace CameronBonde
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public class CamBlasterGuy_Model : Character_Base
+	{
+	}
 }
