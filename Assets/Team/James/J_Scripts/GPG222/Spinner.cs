058bd828e4886e17d787dba594821e640ae34cf5
HerbalsGerbals <1032054@student.sae.edu.au>
2024-10-27 14:34:39 +1100

    Finished Spinner Code

--- Assets/Team/James/J_Scripts/GPG222/Spinner.cs
+++ Assets/Team/James/J_Scripts/GPG222/Spinner.cs
@@ -4,119 +4,150 @@ using TMPro;
 using UnityEngine;
 using Unity.Netcode;
 
-namespace JamesKilpatrick
+public class Spinner : NetworkBehaviour
 {
-    public class Spinner : NetworkBehaviour
+    public Rigidbody spinBody;
+
+    //Spins Game Object 
+    public Transform spinTransform;
+
+    //The Start Point for the Game Object used for its movement
+    public Vector3 targetPosition;
+
+    public Vector3 newTargetPosition;
+
+    //Direction Game Object moves to
+    public Vector3 targetDirection;
+
+    //Speed of turn
+    public float turnspeed;
+
+    //Speed of Movement
+    public float speed;
+
+    //Used to make spinner move towards a Game Object
+    public Transform targetObject;
+
+    public Renderer rend;
+
+    public bool reachedTargetPosition;
+
+    //Starting Position of Spinner used too loop movement
+    public Vector3 startPosition;
+
+    //Current Position of spinner used too check bool
+    public Vector3 currentPosition;
+
+    public enum states
     {
+        Idle,
+        Rotating,
+        Moving
+    }
+    public states state;
 
-        public Rigidbody spinBody;
+    void Start()
+    {
+        StartCoroutine(StateManager());
+        startPosition = transform.position;
+    }
 
-        //Spins Game Object 
-        public Transform spinTransform;
-
-        //The Start Point for the Game Object used for its movement
-        public Vector3 targetPosition;
-
-        public Vector3 newTargetPosition;
-
-        //Direction Game Object moves to
-        public Vector3 targetDirection;
-
-        //Speed of turn
-        public float turnspeed;
-
-        //Speed of Movement
-        public float speed;
-
-        //Used to make spinner move towards a Game Object
-        public Transform targetObject;
-
-        public Renderer rend;
-
-        public bool reachedTargetPosition;
-
-        public enum states
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Players should not have control of changing states
+        if (IsServer)
         {
-            Idle,
-            Rotating,
-            Moving
-        }
-        public states state;
-
-        void Start()
-        {
-            StartCoroutine(StateManager());
-        }
-
-        // Update is called once per frame
-        void FixedUpdate()
-        {
-            //Players should not have control of changing states
-            //if (IsServer)
+            if (state == states.Idle)
             {
-                if (state == states.Idle)
-                {
-                    rend.material.color = Color.white;
-                    // Do Idle
-                }
-                if (state == states.Rotating)
-                {
-
-                    //Starts spin
-                    SpinRotate_ServerToClients_RPC();
-                    rend.material.color = Color.green;
-                }
-                if (state == states.Moving)
-                {
-
-                    //Starts movement 
-                    SpinMovement_ServerToClients_RPC();
-                    rend.material.color = Color.red;
-                }
+                rend.material.color = Color.white;
+                // Do Idle
             }
+            if (state == states.Rotating)
+            {
 
+                //Starts spin
+                SpinRotate_ServerToClients_RPC();
+                ChangeColourToGreen_Rpc();
+            }
+            if (state == states.Moving)
+            {
+
+                //Starts movement 
+                SpinMovement_ServerToClients_RPC();
+                ChangeColourToRed_Rpc();
+            }
         }
-        IEnumerator StateManager()
+        TargetPositionChanger();
+    }
+    IEnumerator StateManager()
+    {
+        state = states.Rotating;
+        yield return new WaitForSeconds(5);
+        state = states.Moving;
+        yield return new WaitForSeconds(10);
+        Debug.Log("Endo");
+        StartCoroutine(StateManager());
+    }
+
+    // Function that runs from the Server TO ALL clients
+    private void SpinMovement_ServerToClients_RPC()
+    {
+        //Used to move towards player marble if wanted
+        if (targetObject)
         {
-            state = states.Rotating;
-            yield return new WaitForSeconds(5);
-            state = states.Moving;
-            yield return new WaitForSeconds(10);
-            Debug.Log("Endo");
-            StartCoroutine(StateManager());
+            targetDirection = (targetObject.position - transform.position).normalized;
         }
-
-        // Function that runs from the Server TO ALL clients
-        [Rpc(SendTo.ClientsAndHost, RequireOwnership = false, Delivery = RpcDelivery.Reliable)]
-        private void SpinMovement_ServerToClients_RPC()
+        //Moves towards a position
+        else
         {
-            if (targetObject)
-            {
-                targetDirection = (targetObject.position - transform.position).normalized;
-            }
-            else
-            {
-                targetDirection = (targetPosition - transform.position).normalized;
-            }
-
-            //need to create code for when target position is reached to go to new position
-
-            if (reachedTargetPosition == true)
-            {
-                targetPosition = newTargetPosition;
-            }
-
-            float angle = Vector3.SignedAngle(transform.forward, targetDirection, transform.up) * turnspeed;
-
-            spinBody.AddRelativeTorque(0, angle, 0);
-            spinBody.AddRelativeForce(0, 0, speed);
+            targetDirection = (targetPosition - transform.position).normalized;
         }
 
-        // Function that runs from the Server TO ALL clients
-        [Rpc(SendTo.ClientsAndHost, RequireOwnership = false, Delivery = RpcDelivery.Reliable)]
-        public void SpinRotate_ServerToClients_RPC()
+        float angle = Vector3.SignedAngle(transform.forward, targetDirection, transform.up) * turnspeed;
+
+        spinBody.AddRelativeTorque(0, angle, 0);
+        spinBody.AddRelativeForce(0, 0, speed);
+    }
+
+    // Function that runs from the Server TO ALL clients
+    public void SpinRotate_ServerToClients_RPC()
+    {
+        spinTransform.Rotate(0, 1f, 0);
+    }
+
+    [Rpc(SendTo.ClientsAndHost, RequireOwnership = false, Delivery = RpcDelivery.Unreliable)]
+    public void ChangeColourToRed_Rpc()
+    {
+        rend.material.color = Color.red;
+    }
+
+    [Rpc(SendTo.ClientsAndHost, RequireOwnership = false, Delivery = RpcDelivery.Unreliable)]
+    public void ChangeColourToGreen_Rpc()
+    {
+        rend.material.color = Color.green;
+    }
+
+    public void TargetPositionChanger()
+    {
+        //Loops Position. this allows for longer routes with the same code
+        if (currentPosition == newTargetPosition)
         {
-            spinTransform.Rotate(0, 1f, 0);
+            reachedTargetPosition = true;
+        }
+        if (currentPosition == startPosition)
+        {
+            reachedTargetPosition = false;
+        }
+
+
+        if (reachedTargetPosition == true)
+        {
+            targetPosition = startPosition;
+        }
+        if (reachedTargetPosition == false)
+        {
+            targetPosition = newTargetPosition;
         }
     }
 }
