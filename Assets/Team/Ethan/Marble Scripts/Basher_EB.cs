043c5a971be43d7c4717c9e4c2f2a2adba08da96
EthanB976 <136556095+EthanB976@users.noreply.github.com>
2024-10-27 18:59:52 +1100

    Added comments to basher script

--- Assets/Team/Ethan/Marble Scripts/Basher_EB.cs
+++ Assets/Team/Ethan/Marble Scripts/Basher_EB.cs
@@ -8,32 +8,49 @@ using UnityEngine;
 
 public class Basher_EB : NetworkBehaviour
 {
+    // basher
     [SerializeField] private GameObject basher;
+
+    // timers for basher, can change in inspecter 
     [SerializeField] private float startTimer;
     [SerializeField] private float downTimer;
     [SerializeField] private float upTimer;
+
+    // base speed, can change in inspecter 
     [SerializeField] private float speed = 10;
+
+    // Basher positions
     [SerializeField] private Transform startPostion, endPostion;
     [SerializeField] private Transform destinationTarget, departTarget;
+    //change delay
     [SerializeField] float changeDirectionDelay;
+    //length of journey
     private float journeyLength;
+    //bool for waiting
     private bool isWaiting;
+    //render to change colour
     public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        //Sets target position
         departTarget = startPostion;
         destinationTarget = endPostion;
 
+        //starts timer
         startTimer = Time.time;
+        // gets its journey distance
         journeyLength = Vector3.Distance(departTarget.position, destinationTarget.position);
+        //starts basher
         isWaiting = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        //if is server start moving
        if (IsServer)
             MoveDown();
     }
@@ -42,14 +59,16 @@ public class Basher_EB : NetworkBehaviour
     {
         if (!isWaiting)
         {
+            //if distance is greater then 0.01 call
             if (Vector3.Distance(transform.position, destinationTarget.position) > 0.01f)
             {
+                //get basher speed
                 float distCovered = (Time.time - startTimer) * speed;
-
+                
                 float fractionOfJourney = distCovered / journeyLength;
-
+                //move to destination
                 transform.position = Vector3.Lerp(departTarget.position, destinationTarget.position, fractionOfJourney);
-
+                // change colour on server
                 if (IsServer && !isWaiting)
                 {
 
@@ -66,6 +85,7 @@ public class Basher_EB : NetworkBehaviour
 
     private void ChangeDestination()
     {
+        //reset postions to new positions
         if (departTarget == endPostion && destinationTarget == startPostion)
         {
             departTarget = startPostion;
@@ -79,6 +99,7 @@ public class Basher_EB : NetworkBehaviour
     }
     IEnumerator changeDeley()
     {
+        //change to new postion and get ready to move
         yield return new WaitForSeconds(changeDirectionDelay);
         ChangeDestination();
         startTimer = Time.time;
@@ -89,6 +110,7 @@ public class Basher_EB : NetworkBehaviour
     [Rpc(SendTo.Server, RequireOwnership = false, Delivery = RpcDelivery.Unreliable)]
     private void ChangeColourBlue_RPC()
     {
+        // change colour to blue
         rend.material.color = Color.blue;
     }
 
