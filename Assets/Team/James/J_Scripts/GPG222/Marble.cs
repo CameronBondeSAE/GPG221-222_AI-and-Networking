1af722aa304b3954d65b3db205d5e0933f5a62bb
HerbalsGerbals <1032054@student.sae.edu.au>
2024-11-13 15:07:38 +1100

    Added Chasing Evil Marble Prefab

--- Assets/Team/James/J_Scripts/GPG222/Marble.cs
+++ Assets/Team/James/J_Scripts/GPG222/Marble.cs
@@ -13,9 +13,9 @@ namespace JamesKilpatrick
         // This one stores a 'float' which is just a number. You can change these in the editor
         public float speed = 25f;
 
-        public Vector3 position;
-        public string soundType;
-        public float radius;
+        //public Vector3 position;
+       // public string soundType;
+       // public float radius;
 
 
         // Functions
@@ -24,8 +24,10 @@ namespace JamesKilpatrick
         {
             // We're talking to the Rigidbody via our variable. Note the dot. This will show you everything that component can do
             rb.AddTorque(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+            
         }
 
+       /*
         public void EmitSound()
         {
             Collider[] results = new Collider[] { };
@@ -41,7 +43,7 @@ namespace JamesKilpatrick
                 }
             }
         }
-
+       */
        
 
     }
