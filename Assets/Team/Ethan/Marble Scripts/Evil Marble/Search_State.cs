5be37c90b97436aa46f9ea9279d06eff5e0300e7
EthanB976 <136556095+EthanB976@users.noreply.github.com>
2024-11-13 19:45:12 +1100

    started on AntAi planner

---
+++ Assets/Team/Ethan/Marble Scripts/Evil Marble/Search_State.cs
@@ -0,0 +1,23 @@
+using Anthill.AI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EB
+{
+    public class Search_State : AntAIState
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
