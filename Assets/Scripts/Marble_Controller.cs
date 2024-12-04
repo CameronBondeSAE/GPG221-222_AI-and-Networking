b465e2d6cc691feb3ae5022349d68748ceb9ea6a
Cameron Bonde <CameronBonde@users.noreply.github.com>
2024-12-04 11:20:07 +1100

    ODIN plugin

--- Assets/Scripts/Marble_Controller.cs
+++ Assets/Scripts/Marble_Controller.cs
@@ -6,11 +6,18 @@ using UnityEngine.Serialization;
 public class Marble_Controller : MonoBehaviour
 {
 	public Marble_Model marbleModel;
+	public KeyCode      jump = KeyCode.Space;
 
-    // Update is called once per frame
-    void Update()
-    {
-	    Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-	    marbleModel.Roll(direction);
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		marbleModel.Roll(direction);
+
+		if (Input.GetKeyDown(jump))
+		{
+			Debug.Log("Controller Jump");
+			marbleModel.Jump_Rpc();
+		}
+	}
 }
--- Assets/Scripts/Marble_Model.cs
+++ Assets/Scripts/Marble_Model.cs
@@ -1,18 +1,27 @@
+using Unity.Netcode;
 using UnityEngine;
 
 [SelectionBase]
-public class Marble_Model : MonoBehaviour
+public class Marble_Model : NetworkBehaviour
 {
 	// Variables
 	// They store information
 	// This one stores which 'Rigidbody' component we want to talk to. They do the physics movement
 	public Rigidbody rb;
 	// This one stores a 'float' which is just a number. You can change these in the editor
+	[Tooltip("The is the rotation speed")]
 	public float speed = 25f;
 	
 	public void Roll(Vector3 direction)
 	{
 		// We're talking to the Rigidbody via our variable. Note the dot. This will show you everything that component can do
 		rb.AddTorque(direction * speed);
+	}
+
+	[Rpc(SendTo.ClientsAndHost, RequireOwnership = true)]
+	public void Jump_Rpc()
+	{
+		Debug.Log("Actual Jump");
+		rb.AddForce(Vector3.up * speed, ForceMode.VelocityChange);
 	}
 }
